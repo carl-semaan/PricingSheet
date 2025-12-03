95520689bdd745f16dd12e0ca66e82f87359830b
Carl <carl.s.semaan@gmail.com>
2025-12-03 19:05:42 +0200

    Add project files.

---
+++ PricingSheet/Flux.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.Tools.Applications.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+
+namespace PricingSheet
+{
+    public partial class Flux
+    {
+        private void Sheet3_Startup(object sender, System.EventArgs e)
+        {
+            SheetButton sheetButton = new SheetButton(
+                "Say Hello",
+                1,
+                1,
+                "Blue",
+                () => System.Windows.Forms.MessageBox.Show("Welcome to the new Pricing Sheet!!!"));
+
+            List<SheetButton> ButtonsList = new List<SheetButton>();
+            ButtonsList.Add(sheetButton);
+
+            var sheet = Globals.ThisWorkbook.Worksheets["Sheet3"];
+            var vstoSheet = Globals.Factory.GetVstoObject(sheet);
+
+            SheetInitialization sheetInitialization = new SheetInitialization(
+                vstoSheet,
+                "Flux",
+                true,
+                ButtonsList
+            );
+
+            sheetInitialization.Run();
+        }
+
+        private void Sheet3_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region VSTO Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(Sheet3_Startup);
+            this.Shutdown += new System.EventHandler(Sheet3_Shutdown);
+        }
+
+        #endregion
+
+    }
+}
---
+++ PricingSheet/SheetInitialization.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Office.Tools.Excel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using ExcelInterop = Microsoft.Office.Interop.Excel;
+using ExcelVSTO = Microsoft.Office.Tools.Excel;
+
+
+namespace PricingSheet
+{
+    public class SheetInitialization
+    {
+        public ExcelVSTO.Worksheet Sheet { get; set; }
+        public string Name { get; set; }
+        public bool ClearOnStartUp { get; set; }
+        public List<SheetButton> SheetButtons { get; set; }
+
+        public SheetInitialization() { }
+
+        public SheetInitialization(ExcelVSTO.Worksheet Sheet, string Name, bool ClearOnStartUp, List<SheetButton> SheetButtons)
+        {
+            this.Sheet = Sheet;
+            this.Name = Name;
+            this.ClearOnStartUp = ClearOnStartUp;
+            this.SheetButtons = SheetButtons;
+        }
+
+        public void Run()
+        {
+            if (Sheet != null)
+            {
+                Sheet.Name = Name;
+
+                if (ClearOnStartUp)
+                    Sheet.Cells.Clear();
+
+                foreach (var btn in SheetButtons)
+                    AddButton(btn);
+            }
+        }
+
+        private void AddButton(SheetButton btn)
+        {
+            ExcelInterop.Range cell = Sheet.Cells[btn.Row, btn.Column] as ExcelInterop.Range;
+
+            int left = (int)cell.Left;
+            int top = (int)cell.Top;
+
+            var button = Sheet.Controls.AddButton(left, top, btn.Width, btn.Height, btn.Name);
+            button.Text = btn.Name;
+
+            button.Click += (s, e) => btn.Action();
+        }
+    }
+
+    public class SheetButton
+    {
+        public string Name { get; set; }
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public string Color { get; set; }
+        public System.Action Action { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public SheetButton() { }
+
+        public SheetButton(string Name, int Row, int Column, string Color, System.Action Action, int Width = 100, int Height = 30)
+        {
+            this.Name = Name;
+            this.Row = Row;
+            this.Column = Column;
+            this.Color = Color;
+            this.Action = Action;
+            this.Width = Width;
+            this.Height = Height;
+        }
+
+    }
+}
