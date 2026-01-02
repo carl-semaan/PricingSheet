09dc2c79cac605ea4fadfc09c0ce0cae126db639
carl-semaan <carl.semaan@melanion.com>
2026-01-02 16:46:18 +0200

    added the data request to bloomberg from the data manager

--- PricingSheetDataManager/Program.cs
+++ PricingSheetDataManager/Program.cs
@@ -1,4 +1,6 @@
-﻿using PricingSheetDataManager.Eurex;
+﻿using PricingSheet.Bloomberg;
+using PricingSheetCore.Models;
+using PricingSheetDataManager.Eurex;
 using PricingSheetDataManager.Euronext;
 using System.Diagnostics;
 using System.Threading.Tasks;
@@ -18,8 +20,57 @@ namespace PricingSheetDataManager
             sw.Stop();
             Console.WriteLine($"Data fetching and parsing completed in {sw.ElapsedMilliseconds / 1000} seconds.");
 
-            // Fetch the necessary data from Bloomberg
+            // Unifying the instruments into a single list
+            List<Instruments> ListedInstruments = GetAllInstruments(eurexInstruments, euronextInstruments);
 
+            //Fetch the ticker data from Bloomberg
+            List<string> DataFieldsTicker = new List<string>() { "OPT_UNDL_TICKER", "CRNCY", "ICB_SUPERSECTOR_NAME" };
+            BloombergDataRequest tickerRequest = new BloombergDataRequest(ListedInstruments.Select(x => $"{x.Ticker}=A {x.ExchangeCode} {x.InstrumentType}").ToList(), DataFieldsTicker);
+
+            var response = await tickerRequest.FetchInstrument();
+
+            //foreach(var r in response.ToList())
+            //{
+            //    ListedInstruments.Where(x => )
+            //}
+
+            List<string> DataFieldsUnderlying = new List<string>() { "SHORT_NAME" };
+            //BloombergDataRequest underlyingRequest = new BloombergDataRequest(response.Result.Select(x => ))
+        }
+
+        private static List<Instruments> GetAllInstruments(List<EurexInstruments> eurexInstruments, List<EuronextInstruments> euronextInstruments)
+        {
+            List<Instruments> ListedInstruments = new List<Instruments>();
+
+            foreach (var instrument in eurexInstruments)
+                ListedInstruments.Add(
+                    new Instruments(
+                        instrument.ProductID,
+                        string.Empty,
+                        string.Empty,
+                        exchangeRegionMapping[Exchanges.Eurex][Region.NoRegion],
+                        "Equity",
+                        string.Empty,
+                        string.Empty,
+                        "Eurex"
+                        )
+                    );
+
+            foreach (var instrument in euronextInstruments)
+                ListedInstruments.Add(
+                    new Instruments(
+                        instrument.Code,
+                        string.Empty,
+                        string.Empty,
+                        exchangeRegionMapping[Exchanges.Euronext][(Region)Enum.Parse(typeof(Region), instrument.Location)],
+                        "Equity",
+                        string.Empty,
+                        string.Empty,
+                        "Euronext"
+                        )
+                    );
+
+            return ListedInstruments;
         }
 
         public static Dictionary<Exchanges, Dictionary<Region, string>> exchangeRegionMapping = new Dictionary<Exchanges, Dictionary<Region, string>>()
